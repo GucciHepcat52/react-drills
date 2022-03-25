a3c76172d538a31fe8e8d8543a7afa9f57eee0fe
Dallin Breen <dallinbreen624@gmail.com>
2022-03-25 12:13:53 -0600

    working on AppThree component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import AppOne from './components/AppOne';
 import AppTwo from './components/AppTwo';
+import AppThree from './components/AppThree';
 
 function App() {
   return (
     <div className="App">
-      
+      <AppThree />
     </div>
   );
 }
--- src/components/AppThree.js
+++ src/components/AppThree.js
@@ -6,14 +6,21 @@ export default class AppThree extends Component {
         super();
         this.state = {
             food: foodData.foods,
-            userInput: ""
         }
+        this.findItem = this.findItem.bind(this);
+    }
+
+    findItem(value) {
+        const foundItem = this.state.foodData.foods.filter((item) => item === value)
+        return foundItem;
     }
 
     render() {
         return (
             <div>
-                
+                <h2>App Three</h2>
+                <input type="text" onChange={(e) => this.findItem(e.target.value)}/>
+                <h3>{this.findItem}</h3>
             </div>
         )
     }
